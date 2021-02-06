c47c0eb0011b273f981c2e99e8bab4678ab10dfd
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-02-06 19:39:54 +0100

    Add time and version in test-nodejs-app output

--- apps/knative/scripts/test-nodejs-app.sh
+++ apps/knative/scripts/test-nodejs-app.sh
@@ -8,10 +8,16 @@ TOTAL_REQ=20
 
 while true; do
   for i in $(seq 1 $TOTAL_REQ); do
+    tstart=$(date +%s.%N)
+    content=$(curl -s $URL)
+    tend=$(date +%s.%N)
+    tconsumed=$(echo "scale=2; ($tend-$tstart)/1" |bc -l)
+    count=$(echo $content | grep -ic version)
+    version=$(echo $content | egrep -o "Version.*?0")
+
     printf "Request #$i: "
-    count=$(curl -s $URL | grep -ic version)
     if [ "$count" -eq "1" ]; then
-      printf "${GREEN}OK${NC}\n"
+      printf "${GREEN}OK${NC} | ${version} | Time: ${tconsumed}s\n"
     else
       printf "${RED}KO${NC}\n"
     fi
